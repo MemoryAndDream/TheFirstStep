d6a4c3ced9cb32103b22504fc49df071fa25fd73
MemoryAndDream <312141830@qq.com>
2018-10-29 23:55:11 +0800

    up

--- word_parser/docx_test.py
+++ word_parser/docx_test.py
@@ -4,10 +4,17 @@
 # @Email   : 312141830@qq.com
 # @File    : docx_test.py
 import docx
+import sys
+import pickle
+import re
+import  codecs
+import string
+import shutil
+from win32com import client as wc
 
 def docx_try():
 
-    doc=docx.Document(r'C:\Users\ASUS\Desktop\1.docx')
+    doc=docx.Document(r'C:\Users\ASUS\Desktop\3.docx')
     for p in doc.paragraphs:
 
         print(p.text)
@@ -20,5 +27,14 @@ def docx_try():
 
                 print(c.text)
 
+def convert():
+    word = wc.Dispatch('Word.Application')
+    doc = word.Documents.Open(r'C:\Users\ASUS\Desktop\3.doc')        # 目标路径下的文件
+    doc.SaveAs(r'C:\Users\ASUS\Desktop\3.docx', 12, False, "", True, "", False, False, False, False)  # 转化后路径下的文件
+    doc.Close()
+    word.Quit()
+
+
 if __name__ == '__main__':
+#    convert()
     docx_try()
