e7d5af05da5e4d8d7c858aa390b6578b676096ac
MemoryAndDream <312141830@qq.com>
2018-11-05 19:47:41 +0800

    up

--- congfujiance/zhuce.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-File Name：     zhuce
-Description :
-Author :       meng_zhihao
-date：          2018/11/2
-"""
-import win32api
-print win32api.GetVolumeInformation("C:\\")[1]
